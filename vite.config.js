30d3d56d1f3491ca9c5d27d9c49cdd8f2ffdb5ba
linnjoha <linn.wallin@hotmail.com>
2024-10-14 10:59:10 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true,
-    strictPort: true,
-    port: 5173,
-  },
+  base: "/github-actions",
+  // server: {
+  //   watch: {
+  //     usePolling: true,
+  //   },
+  //   host: true,
+  //   strictPort: true,
+  //   port: 5173,
+  // },
 });
