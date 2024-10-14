f0c76a7f9ad0bf662279202eab3ef36a461b1516
linnjoha <linn.wallin@hotmail.com>
2024-10-14 12:35:29 +0200

    config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/timer/",
-  // server: {
-  //   watch: {
-  //     usePolling: true,
-  //   },
-  //   host: true,
-  //   strictPort: true,
-  //   port: 5173,
-  // },
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/timer/";
+  }
+
+  return config;
 });
